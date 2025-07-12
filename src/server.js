02ac621e29ec5d3b9104102c67406566161becb5
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-07-12 13:18:04 +0300

    work with swagger documentation

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import contactRouter from "./router/contacts.js";
 import authRouter from "./router/auth-router.js";
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 export const setupServer = () => {
   const app = express();
@@ -17,6 +18,8 @@ export const setupServer = () => {
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use(cookieParser());
   app.use(logger);
+
+  app.use("/api-docs", swaggerDocs());
 
 app.use('/auth', authRouter);
   app.use("/contacts", contactRouter);
